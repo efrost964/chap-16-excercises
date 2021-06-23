b44182909f52db2813f05be87262e0a138fd4da5
The_Lump <thelump@lump.com>
2021-06-23 08:47:06 -0400

    first draft

---
+++ chap-16-studio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace chap_16_studio
+{
+    class Program
+    {
+        static double Division(double x, double y)
+        {
+            try
+            {
+                double answer = x / y;
+                return answer;
+            }
+            catch(DivideByZeroException) 
+            {
+                Console.WriteLine("Cannot divide by 0. Please reinput data correctly.");
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Division!");
+            Console.WriteLine("Please input a number:");
+            double num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Please input another number:");
+            double num2 = Double.Parse(Console.ReadLine());
+            double answer = Division(num1, num2);
+            if (answer > 0)
+            {
+                Console.WriteLine(answer);
+            }
+
+
+        }
+    }
+}
